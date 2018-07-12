a04492c35f68e29888e95aa60cf572cd0f6a2891
Matt Farmer <mrfarmer777@gmail.com>
2018-07-12 18:45:53 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  #callback, before you even TRY to save anything, before you validate it
+  before_validation :make_title_case
+
+  #do this after it's validated, but before it's saved.
+  #before_save :email_author_about_post
 
   private
 
